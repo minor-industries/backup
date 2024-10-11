89348bda2390e63d1eb7fd183f4b78acec111cb9
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-10-11 10:17:31 -0700

    WIP: new profile

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/peterh/liner"
 	"github.com/pkg/errors"
@@ -10,12 +11,52 @@ type ProfileOptions struct {
 	Profile string `short:"p" long:"profile" description:"Profile to use" required:"true"`
 }
 
+var fields = []string{
+	"AWS_ACCESS_KEY_ID",
+	"AWS_SECRET_ACCESS_KEY",
+	"RESTIC_REPOSITORY",
+	"RESTIC_PASSWORD",
+}
+
+func readField(name string) (string, error) {
+	line := liner.NewLiner()
+	defer line.Close()
+
+	line.SetCtrlCAborts(true)
+	res, err := line.Prompt(name + "=")
+	if err != nil {
+		return "", errors.Wrap(err, "get line")
+	}
+
+	return res, nil
+}
+
 type NewCommand struct {
 	ProfileOptions
 }
 
 func (cmd *NewCommand) Execute(args []string) error {
-	fmt.Printf("New command called with profile: %s\n", cmd.Profile)
+	fmt.Println("new profile")
+
+	result := map[string]string{}
+
+	for _, field := range fields {
+		f, err := readField(field)
+		if err != nil {
+			return errors.Wrap(err, "read field")
+		}
+		if f != "" {
+			result[field] = f
+		}
+	}
+
+	out, err := json.Marshal(result)
+	if err != nil {
+		return errors.Wrap(err, "marshal")
+	}
+
+	fmt.Println(string(out))
+
 	return nil
 }
 
@@ -40,8 +81,6 @@ type EditCommand struct {
 }
 
 func (cmd *EditCommand) Execute(args []string) error {
-	fmt.Println("edit", cmd.Profile)
-
 	line := liner.NewLiner()
 	defer line.Close()
 
